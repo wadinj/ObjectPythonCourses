fe5c71f0008b1da99053e8890880bf5dcefdcc29
Jonathan Wadin <wadin.jonathan@gmail.com>
2020-11-18 19:41:55 +0100

    (update) TP2

--- first_tp.py
+++ first_tp.py
@@ -12,4 +12,18 @@
 # 1. Permettre au joueur de tout relancer sans choisir un emoji
 # 2. Prendre en paramètre (args) la taille de la machine à sous
 # 3. Développer une IA (la plus simple possible) permettant d'ajouter des joueurs "machine"
-# 4. Ajouter un menu contextuel présentant le Casio EPSI Lille et permettant de naviguer vers la machine à sous
+# 4. Ajouter un menu contextuel présentant le Casino EPSI Lille et permettant de naviguer vers la machine à sous
+
+# Remplacer le deuxième joueur par un joueur IA 
+# Ajouter un budget lors du lancement du casino (Crédits: 10)
+# Lors du lancement de la machine à sous, demandez le nombre de crédit que vous souhaitez jouer
+# Ajouter un menu contextuel avec les options des différents jeux (pour l'instant juste la machine à sous) ainsi que le nombre de crédits du joueur
+# Permettre de revenir au menu à chaque fin de partie de machine à sous
+
+# Développez le jeu BlackJack
+# Fonctionnement :
+    # > Demander le montant de la mise
+    # > Tirer une carte pour le croupier et l'afficher
+    # > Tirer deux cartes pour le joueur et lui montrer, proposer si le joueur souhaite encore des cartes 
+    # > Si oui, tirer des cartes tant qu'il souhaite. Si le joueur dépasse 21 il a perdu.
+    # > Si le joueur s'arrête, tirer des cartes pour le croupier jusqu'a sa victoire
